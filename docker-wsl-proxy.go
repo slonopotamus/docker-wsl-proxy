d6302ad0258d4c9904649d5a7fa04f7d7e425fda
Marat Radchenko <marat@slonopotamus.org>
2022-01-30 18:48:20 +0300

    Add infrastructure for bind path rewriting

--- docker-wsl-proxy.go
+++ docker-wsl-proxy.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/Microsoft/go-winio"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	specs "github.com/opencontainers/image-spec/specs-go/v1"
+	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -67,15 +73,7 @@ func main() {
 	}
 
 	proxy := httputil.NewSingleHostReverseProxy(&proxyTarget)
-	origDirector := proxy.Director
-	proxy.Director = func(req *http.Request) {
-		if strings.HasSuffix(req.RequestURI, "/containers/create") {
-			// TODO: rewrite mount paths
-			origDirector(req)
-		} else {
-			origDirector(req)
-		}
-	}
+	proxy.Director = proxyDirector(proxy.Director)
 
 	// TODO: we need to connect to docker running within WSL2.
 	proxy.Transport = createTransport("npipe", "//./pipe/docker_engine_linux")
@@ -93,4 +91,51 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type configWrapper struct {
+	*container.Config
+	HostConfig       *container.HostConfig
+	NetworkingConfig *network.NetworkingConfig
+	Platform         *specs.Platform
+}
+
+func proxyDirector(origDirector func(*http.Request)) func(req *http.Request) {
+	return func(req *http.Request) {
+		if strings.HasSuffix(req.RequestURI, "/containers/create") {
+			err := handleContainerCreate(req)
+			if err != nil {
+				panic(err)
+			}
+			origDirector(req)
+		} else {
+			origDirector(req)
+		}
+	}
+}
+
+func handleContainerCreate(req *http.Request) error {
+	buf := make([]byte, req.ContentLength)
+	_, err := io.ReadFull(req.Body, buf)
+	if err != nil {
+		return err
+	}
+
+	config := configWrapper{}
+	err = json.Unmarshal(buf, &config)
+	if err != nil {
+		return err
+	}
+
+	// TODO: rewrite config.HostConfig.Binds
+
+	buf, err = json.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	req.Body = io.NopCloser(bytes.NewReader(buf))
+	req.ContentLength = int64(len(buf))
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,18 @@ module docker-wsl-proxy
 
 go 1.17
 
-require github.com/Microsoft/go-winio v0.5.1
+require (
+	github.com/Microsoft/go-winio v0.5.1
+	github.com/docker/docker v20.10.12+incompatible
+	github.com/opencontainers/image-spec v1.0.2
+)
 
-require golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+require (
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	gotest.tools/v3 v3.1.0 // indirect
+)
