82e71755cbc4c2fb4d26ebe16e50a03b4952f4f7
Marat Radchenko <marat@slonopotamus.org>
2022-02-16 12:47:14 +0300

    resolves #1 implement bind-mount path rewriting (#3)

---
+++ docker-wsl-proxy_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestRewriteBindToWSL(t *testing.T) {
+	testCases := []struct {
+		input, expected string
+	}{
+		// See https://github.com/docker/for-win/issues/6628#issuecomment-635906394
+		{`C:\windows\system32:/c`, `/mnt/c/windows/system32:/c`},
+		{`C://windows/system32:/c`, `/mnt/c/windows/system32:/c`},
+		{`//C/windows/system32:/c`, `/mnt/c/windows/system32:/c`},
+		{`/C/windows/system32:/c`, `/mnt/c/windows/system32:/c`},
+		{`/mnt/c/windows/system32:/c`, `/mnt/c/windows/system32:/c`},
+		// Compatibility with Docker Desktop
+		{`/host_mnt/c/windows/system32:/c`, `/mnt/c/windows/system32:/c`},
+		// Additional cases
+		{`C:\:/c`, `/mnt/c:/c`},
+		{`C:/:/c`, `/mnt/c:/c`},
+		{`/proc:/proc`, `/proc:/proc`},
+		{`/:/host`, `/:/host`},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			actual := rewriteBindToWSL(tc.input)
+			assert.Equal(t, actual, tc.expected)
+		})
+	}
+}
+
+func TestRewriteBindToWindows(t *testing.T) {
+	testCases := []struct {
+		input, expected string
+	}{
+		{`/mnt/c/windows/system32:/c`, `C:\windows\system32:/c`},
+		{`/proc:/proc`, `/proc:/proc`},
+		{`/:/host`, `/:/host`},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			actual := rewriteBindToWindows(tc.input)
+			assert.Equal(t, actual, tc.expected)
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/docker/docker v20.10.12+incompatible
 	github.com/docker/go-connections v0.4.0
 	github.com/opencontainers/image-spec v1.0.2
+	gotest.tools v2.2.0+incompatible
 )
 
 require (
@@ -23,6 +24,7 @@ require (
 	github.com/moby/sys/mountinfo v0.5.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/runc v1.1.0 // indirect
+	github.com/opencontainers/selinux v1.10.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
